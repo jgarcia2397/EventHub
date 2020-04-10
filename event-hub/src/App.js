759298a43aec03099a695db1040ca022321df91d
Josh Garcia <josh2397garcia@gmail.com>
2020-04-10 14:39:57 -0400

    Move Calendar.js to Containers since it will have to be aware of state

--- event-hub/src/App.js
+++ event-hub/src/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
 
 import Layout from './hoc/Layout/Layout';
-import Calendar from './components/Calendar/Calendar';
+import Calendar from './containers/Calendar/Calendar';
 
 const App = () => {
   return (
