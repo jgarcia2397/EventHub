d0be2a93210ba51ce034725c11c5ac82a5d20f24
Josh Garcia <josh2397garcia@gmail.com>
2020-04-14 18:17:11 -0400

    Set up Routes in App.js

--- event-hub/src/App.js
+++ event-hub/src/App.js
@@ -1,13 +1,16 @@
 import React from 'react';
+import { Route } from 'react-router-dom';
 
 import Layout from './hoc/Layout/Layout';
 import EventHub from './containers/EventHub/EventHub';
+import EventForm from './containers/EventForm/EventForm';
 
 const App = () => {
   return (
     <div className="App">
       <Layout>
-        <EventHub />
+        <Route path="/" exact component={EventHub} />
+        <Route path="/createEventForm" component={EventForm} />
       </Layout>
     </div>
   );
