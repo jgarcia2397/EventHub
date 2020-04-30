503dce65fecc67acca72945fc26e5e495311beba
Josh Garcia <josh2397garcia@gmail.com>
2020-04-30 15:15:47 -0400

    Work on reducer code for selectMonth and selectYear

--- event-hub/src/store/reducers/reducer.js
+++ event-hub/src/store/reducers/reducer.js
@@ -4,12 +4,35 @@ import * as actionTypes from '../actions/actionTypes';
 
 const initialState = {
     dateObject: moment(),
+    monthList: moment.months(),
     showMonthSelector: false,
     showYearSelector: false
 };
 
 const reducer = (state = initialState, action) => {
+    switch(action.type) {
+        case actionTypes.SELECT_MONTH:
+            let monthNo = state.monthList.indexOf(action.monthName); 
+            let updatedDateObject = Object.assign({}, state.dateObject);
+            updatedDateObject = moment(state.dateObject).set("month", monthNo); 
+            
+            return {
+                ...state,
+                dateObject: updatedDateObject,
+                showMonthSelector: !state.showMonthSelector
+            };
+        case actionTypes.SELECT_YEAR:
+            let updatedDateObject = Object.assign({}, state.dateObject);
+            updatedDateObject = moment(state.dateObject).set("year", year); 
 
+            return {
+                ...state,
+                dateObject: updatedDateObject,
+                showMonthSelector: !state.showMonthSelector
+            };
+        default:
+            return state;
+    }
 };
 
 export default reducer;
