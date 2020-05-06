c9385991aec6a5766dcd231ed9e44c871e952bdf
Josh Garcia <josh2397garcia@gmail.com>
2020-05-06 13:21:58 -0400

    Add action and reducer case for logging out automatically after one hour

--- event-hub/src/store/actions/actionTypes.js
+++ event-hub/src/store/actions/actionTypes.js
@@ -23,6 +23,7 @@ export const DELETE_EVENT_FAILED = 'DELETE_EVENT_FAILED';
 export const AUTH_START = 'AUTH_START';
 export const AUTH_SUCCESS = 'AUTH_SUCCESS';
 export const AUTH_FAILED = 'AUTH_FAILED';
+export const AUTH_LOGOUT = 'AUTH_LOGOUT';
 
 // export const ON_DAY_CLICK = 'ON_DAY_CLICK';
 // export const ON_CREATE_EVENT_CANCEL = 'ON_CREATE_EVENT_CANCEL';
