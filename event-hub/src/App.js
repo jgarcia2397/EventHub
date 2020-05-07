58534fd1de2916a9a0e8662843d3e8ecd7615058
Josh Garcia <josh2397garcia@gmail.com>
2020-05-07 16:04:10 -0400

    Guarding routes from non-authenticated users

--- event-hub/src/App.js
+++ event-hub/src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { Route, withRouter } from 'react-router-dom';
+import { Route, withRouter, Redirect } from 'react-router-dom';
 import { connect } from 'react-redux';
 
 import * as actions from './store/actions/index';
@@ -9,6 +9,7 @@ import EventForm from './containers/EventForm/EventForm';
 import GroupChats from './containers/GroupChats/GroupChats';
 import Auth from './containers/Auth/Auth';
 import Logout from './containers/Auth/Logout/Logout';
+import Auxiliary from './hoc/Auxiliary/Auxiliary';
 
 class App extends Component {
   componentDidMount() {
@@ -16,19 +17,43 @@ class App extends Component {
   }
 
   render () {
+    let routes = (
+      <Auxiliary>
+        <Route path="/createEventForm" component={EventForm} />
+        <Route path="/auth" component={Auth} />
+        <Route path="/" exact component={EventHub} />
+        <Redirect to="/" />
+      </Auxiliary>
+    );
+
+    if (this.props.isAuthenticated) {
+      routes = (
+        <Auxiliary>
+          <Route path="/createEventForm" component={EventForm} />
+          <Route path="/auth" component={Auth} />
+          <Route path="/chats" component={GroupChats} />
+          <Route path="/logout" component={Logout} />
+          <Route path="/" exact component={EventHub} />
+          <Redirect to="/" />
+        </Auxiliary>
+      );
+    }
+
     return (
       <div className="App">
         <Layout>
-            <Route path="/createEventForm" component={EventForm} />
-            <Route path="/auth" component={Auth} />
-            <Route path="/chats" component={GroupChats} />
-            <Route path="/logout" component={Logout} />
-            <Route path="/" exact component={EventHub} />
+          {routes}
         </Layout>
       </div>
     );
   }
 }
+
+const mapStateToProps = state => {
+  return {
+    isAuthenticated: state.auth.token !== null
+  };
+};
 
 const mapDispatchToProps = dispatch => {
   return {
@@ -36,4 +61,4 @@ const mapDispatchToProps = dispatch => {
   };
 };
 
-export default withRouter(connect(null, mapDispatchToProps)(App));
+export default withRouter(connect(mapStateToProps, mapDispatchToProps)(App));
